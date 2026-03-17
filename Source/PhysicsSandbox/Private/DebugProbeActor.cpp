082b91a10a1af7cc6415fa7597302830bb00155e
ManuDS <ManudDS@email.com>
2026-03-17 09:49:31 +0100

    BASE: Add empty DebugProbe Actor

--- Source/PhysicsSandbox/Private/DebugProbeActor.cpp
+++ Source/PhysicsSandbox/Private/DebugProbeActor.cpp
@@ -8,35 +8,14 @@
 ADebugProbeActor::ADebugProbeActor()
 {
 	PrimaryActorTick.bCanEverTick = true;
-
-	// Crear el componente de malla
-	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
-
-	// Hacer que sea el root del actor
-	SetRootComponent(MeshComp);
-
-	// Opcional: desactivar simulación física
-	MeshComp->SetSimulatePhysics(false);
 }
 
 void ADebugProbeActor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	StartLocation = GetActorLocation();
-
-	UE_LOG(LogTemp, Warning, TEXT("DebugProbeActor BeginPlay. StartLocation = %s"), *StartLocation.ToString());
 }
 
 void ADebugProbeActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	const float Time = GetWorld() ? GetWorld()->GetTimeSeconds() : 0.0f;
-	const float OffsetZ = FMath::Sin(Time * Speed) * Amplitude;
-
-	FVector NewLocation = StartLocation;
-	NewLocation.Z += OffsetZ;
-
-	SetActorLocation(NewLocation);
 }
--- Source/PhysicsSandbox/Private/DebugProbeActor.h
+++ Source/PhysicsSandbox/Private/DebugProbeActor.h
@@ -6,36 +6,17 @@
 #include "GameFramework/Actor.h"
 #include "DebugProbeActor.generated.h"
 
-class UStaticMeshComponent;
-
 UCLASS()
 class PHYSICSSANDBOX_API ADebugProbeActor : public AActor
 {
 	GENERATED_BODY()
-
-public:
+	
+public:	
 	ADebugProbeActor();
 
 protected:
 	virtual void BeginPlay() override;
 
-public:
+public:	
 	virtual void Tick(float DeltaTime) override;
-
-private:
-	// Altura base desde la que oscila
-	FVector StartLocation;
-
-	// Componente visible del actor (siempre existe)
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "DebugProbe", meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UStaticMeshComponent> MeshComp;
-
-public:
-	// Velocidad de oscilación (rad/s)
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DebugProbe")
-	float Speed = 2.0f;
-
-	// Amplitud en cm
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DebugProbe")
-	float Amplitude = 50.0f;
 };
