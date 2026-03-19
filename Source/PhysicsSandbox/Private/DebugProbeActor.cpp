82d57ed1705999185d1d7e881929351400a56f78
ManuDS <ManudDS@email.com>
2026-03-19 15:13:23 +0100

    Sphere bouncing. Poor behaviour in rebounds.

--- Source/PhysicsSandbox/Private/DebugProbeActor.cpp
+++ Source/PhysicsSandbox/Private/DebugProbeActor.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "DebugProbeActor.h"
+
 #include "Engine/World.h"
 #include "GameFramework/Actor.h"
 
@@ -14,6 +15,11 @@ void ADebugProbeActor::BeginPlay()
 {
     UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
     Super::BeginPlay();
+
+   // FVector StartLocation = GetActorLocation();
+   // StartLocation.Z = Z_o;
+   // SetActorLocation(StartLocation);
+
     if (GEngine)
     {
         GEngine->AddOnScreenDebugMessage(
@@ -36,15 +42,24 @@ void ADebugProbeActor::Tick(float DeltaTime)
     CurrentLocation.Z += v_Z * DeltaTime;
     SetActorLocation(CurrentLocation);
 
+    if (CurrentLocation.Z <= GroundZ)
+    {
+        CurrentLocation.Z = 0.0f;
+        if (v_Z < 0.0f) {
+            v_Z = -v_Z * 0.8f; // rebote con pérdida
+        }
+    }
+
     if (GEngine)
     {
         GEngine->AddOnScreenDebugMessage(
-            -1,
+            1,
             0.0f,
             FColor::Green,
             FString::Printf(
-                TEXT("Z: %.2f | Vz: %.2f | Az: %.2f"),
-                CurrentLocation.Z, v_Z, a_Z)
+                TEXT("Z: %.2f | vZ: %.2f | aZ: %.2f\nRunningTime: %.2f"),
+                CurrentLocation.Z, v_Z, a_Z, RunningTime
+            )
         );
     }
 }
--- Source/PhysicsSandbox/Public/DebugProbeActor.h
+++ Source/PhysicsSandbox/Public/DebugProbeActor.h
@@ -19,13 +19,16 @@ protected:
 
 public:
     virtual void Tick(float DeltaTime) override;
+
 private:
     // Actor::Tick variables
     int FrameCount;
     float RunningTime = 0.0f;
-    float LogTimer = 0.0f;
 
     // Physical variables
-    float v_Z = 50.0f; 
-    float a_Z = 15.0f;
+    float Z_o = 500.0f;
+    float v_Z = 00.0f; 
+    float a_Z = -980.0f; // g = 980 cm/s^2 Para gravedad terrestre en unreal
+    float Restitution = 0.8f;
+    float GroundZ = 0.01f;
 };
