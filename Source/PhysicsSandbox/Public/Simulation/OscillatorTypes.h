9ab3cc82fe3f1ae76101c462791735fb71f4c881
ManuDS <ManudDS@email.com>
2026-05-12 12:08:27 +0200

    Add reusable simulation operators for oscillator types

--- Source/PhysicsSandbox/Public/Simulation/OscillatorTypes.h
+++ Source/PhysicsSandbox/Public/Simulation/OscillatorTypes.h
@@ -2,6 +2,22 @@
 
 #pragma once
 
+template<typename DerivedType>
+struct TMathOperators
+{
+    DerivedType operator*(double Scalar) const
+    {
+        DerivedType Result = static_cast<const DerivedType&>(*this);
+        Result.Scale(Scalar);
+        return Result;
+    }
+
+    friend DerivedType operator*(double Scalar, const TMathOperators& Value)
+    {
+        return static_cast<const DerivedType&>(Value) * Scalar;
+    }
+};
+
 struct FOscillatorParams
 {
     double Mass = 1.0;
@@ -10,32 +26,30 @@ struct FOscillatorParams
     double RestPosition = 0.0;
 };
 
-struct FOscillatorState
+struct FOscillatorState : public TMathOperators<FOscillatorState>
 {
     double Position = 0.0;
     double Velocity = 0.0;
     double Acceleration = 0.0;
 
-    FOscillatorState operator*(double Scalar) const
+    void Scale(double Scalar)
     {
-        FOscillatorState Result = *this;
-
-        Result.Position *= Scalar;
-        Result.Velocity *= Scalar;
-        Result.Acceleration *= Scalar;
-
-        return Result;     
-    }
-
-    friend FOscillatorState operator*(double Scalar, const FOscillatorState& State)
-    {
-        return State * Scalar;
+        Position *= Scalar;
+        Velocity *= Scalar;
+        Acceleration *= Scalar;
     }
 };
 
-struct FOscillatorForces
+struct FOscillatorForces : public TMathOperators<FOscillatorForces>
 {
     double SpringForce = 0.0;
     double DampingForce = 0.0;
     double NetForce = 0.0;
+
+    void Scale(double Scalar)
+    {
+        SpringForce *= Scalar;
+        DampingForce *= Scalar;
+        NetForce *= Scalar;
+    }
 };
